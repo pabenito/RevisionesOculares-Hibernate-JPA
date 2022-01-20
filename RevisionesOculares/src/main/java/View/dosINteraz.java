fd200ebd0d29ebbacc105b325bc0aca8b866190a
Laura Plaza <amigo@DESKTOP-STAK0GE.(none)>
2022-01-20 16:47:12 +0100

    listener de la segunda interfaz (NO SÉ CONECTARLO CON LA BBDD)

--- RevisionesOculares/src/main/java/View/dosINteraz.java
+++ RevisionesOculares/src/main/java/View/dosINteraz.java
@@ -9,6 +9,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Calendar;
 
+import static java.lang.System.exit;
+
 public class dosINteraz {
     private Eye seleccionado;
     private Client clienteSeleccionado;
@@ -30,30 +32,89 @@ public class dosINteraz {
     private JButton bLimpiar;
     private JButton bSalir;
 
-    public dosINteraz(Client cliente,Eye revision) {
-       seleccionado=  revision; ;  // creo que desde Revision ocular deería pasarle esto como consturctor
-        clienteSeleccionado = cliente;
+    public dosINteraz() {
+        seleccionado = null;
+
+          // al crearse el constructor es como el LOAD en C#
+        // cargamos la tabla conectándonos a la bbdd
+
 
         bLimpiar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 seleccionado = null;
-                mostrarSeleccianado();
+                mostrarSeleccionado();
             }
         });
         bAñadir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                try {
+                    // falta conectar con la bbdd
+
+                    Eye prueba = new Eye();
+                    prueba.setOdAdicion(seleccionado.getOdAdicion());
+                    prueba.setOdAgudeza(seleccionado.getOdAgudeza());
+                    prueba.setOdCilindro(seleccionado.getOdCilindro());
+                    prueba.setOdEsfera(seleccionado.getOdEsfera());
+
+                    prueba.setOiAdicion(seleccionado.getOiAdicion());
+                    prueba.setOiAgudeza(seleccionado.getOiAgudeza());
+                    prueba.setOiCilindro(seleccionado.getOiCilindro());
+                    prueba.setOiEsfera(seleccionado.getOiEsfera());
+
+                    // miBd. .inssert(prueba)
+
+
+                    table1.clearSelection();
+                    //cargamos la bd de nuevo
+
+
+                    seleccionado = null;
+                    mostrarSeleccionado();
+
+
+                } catch (Exception ex) {
+                    System.err.println("ERROR " + ex.getMessage()); // esto hay que cambiarlo parar que salga una pantallita
+                }
+            }
+        });
+        bBorrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                exit(-1);
+            }
+        });
+        bBorrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    // falta conectar con la bbdd
+
+
+
+                //   mibd.borrar(seleccionado)
+                    table1.clearSelection();
+                    //cargamos la bd de nuevo
+                    seleccionado = null;
+                    mostrarSeleccionado();
+
+
+                } catch (Exception ex) {
+                    System.err.println("ERROR " + ex.getMessage()); // esto hay que cambiarlo parar que salga una pantallita
+                }
             }
         });
     }
 
 
-    public void mostrarSeleccianado(){
-        if (seleccionado == null){
+    public void mostrarSeleccionado() {
+        if (seleccionado == null) {
             // podriarecargar el cread
 
+            table1.clearSelection();
 
             tID_ADICION.setText("");
             tID_AGUDEZA.setText("");
@@ -63,17 +124,20 @@ public class dosINteraz {
             tOD_AGUDEZA.setText("");
             tOD_CILINDRO.setText("");
             tOD_AGUDEZA.setText("");
+            tOD_ESFERA.setText("");
 
 
-        }else{
-            tID_ADICION.setText((seleccionado.getOiAdicion());
-            tID_AGUDEZA.setText("");
-            tID_CILINDRO.setText("");
-            tID_ESFERA.setText("");
-            tOD_ADICION.setText("");
-            tOD_AGUDEZA.setText("");
-            tOD_CILINDRO.setText("");
-            tOD_AGUDEZA.setText("");
+        } else {
+
+
+            tID_ADICION.setText("" + seleccionado.getOiAdicion());
+            tID_AGUDEZA.setText("" + seleccionado.getOiAgudeza());
+            tID_CILINDRO.setText("" + seleccionado.getOdCilindro());
+            tID_ESFERA.setText("" + seleccionado.getOdEsfera());
+            tOD_ADICION.setText("" + seleccionado.getOdAdicion());
+            tOD_AGUDEZA.setText("" + seleccionado.getOdAgudeza());
+            tOD_CILINDRO.setText("" + seleccionado.getOdCilindro());
+            tOD_AGUDEZA.setText("" + seleccionado.getOdAgudeza());
         }
     }
 
@@ -182,5 +246,6 @@ public class dosINteraz {
     public JComponent $$$getRootComponent$$$() {
         return panel2;
     }
+
     //private Calendar calendar = Calendar.getInstance();
 }
